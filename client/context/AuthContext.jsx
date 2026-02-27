4cd6623201faad37fc0efff5c761f642e3bf53ba
Rupinder-24 <rupindermurmu@gmail.com>
2026-02-28 05:25:24 +0530

    updating frontend backend url

--- client/context/AuthContext.jsx
+++ client/context/AuthContext.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const backendUrl =import.meta.VITE_BACKEND_URL;
+const backendUrl ="https://quickchat-app-1-file.onrender.com";
 axios.defaults.baseURL=backendUrl;
 
 export const AuthContext = createContext();
